bebd7270c82010b8a35d9a984d450cf92220c7b5
dmazanko <mazankodm@gmail.com>
2017-01-21 16:03:12 +0300

    note

---
+++ circle_squre.py
@@ -0,0 +1,25 @@
+import math
+
+x = raw_input('input x= ')
+figure = raw_input('figure is :')
+try:
+    x = int(x)
+    figure = str(figure)
+except:
+    print "error "
+    exit(1)
+if x <= 0:
+    print 'error'
+    exit(1)
+# elif figure!=('squre','circle'):
+#     print 'error figure is %s or x is below zero %s' % (figure, x)
+#     exit(1)
+if figure == 'square':
+    S = x * x
+elif figure == 'circle':
+    S = math.pi * x * x
+else:
+    print 'error'
+    exit(1)
+print "The area is %.2f" % S
+exit(0)
